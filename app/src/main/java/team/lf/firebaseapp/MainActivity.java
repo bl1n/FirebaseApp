e40f76e204b7f78d28b207d240b1b9f52ca9b1b8
bl1n <sukhanov87@gmail.com>
2019-10-06 16:44:43 +0300

    1

--- app/src/main/java/team/lf/firebaseapp/MainActivity.java
+++ app/src/main/java/team/lf/firebaseapp/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, EmailPasswordActivity.class));
+        startActivity(new Intent(this, GoogleSignInActivity.class));
+
     }
 
 
