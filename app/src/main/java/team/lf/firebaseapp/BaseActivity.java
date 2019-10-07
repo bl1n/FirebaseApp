f42b57342cf15daf59ed483911b8f90a25c65660
bl1n <sukhanov87@gmail.com>
2019-10-07 11:23:06 +0300

    optimised

--- app/src/main/java/team/lf/firebaseapp/BaseActivity.java
+++ app/src/main/java/team/lf/firebaseapp/BaseActivity.java
@@ -1,9 +1,6 @@
 package team.lf.firebaseapp;
 
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,12 +26,6 @@ public class BaseActivity extends AppCompatActivity {
         }
     }
 
-    public void hideKeyboard(View view) {
-        final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        if (imm != null) {
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-    }
 
     @Override
     public void onStop() {
--- app/src/main/java/team/lf/firebaseapp/MainActivity.java
+++ app/src/main/java/team/lf/firebaseapp/MainActivity.java
@@ -4,24 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -29,7 +22,7 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
-    private static final int RC_SIGN_IN = 9001;
+    private static final int RC_SIGN_IN = 999;
 
 
     private EditText mEmailField;
@@ -77,19 +70,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
-                // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 signInWithGoogle(account);
             } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e);
-                // [START_EXCLUDE]
                 updateUI(null);
-                // [END_EXCLUDE]
             }
         }
     }
@@ -102,27 +90,19 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         showProgressDialog();
 
-        // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success");
                         FirebaseUser user = mAuth.getCurrentUser();
                         updateUI(user);
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
                         Toast.makeText(MainActivity.this, "Authentication failed.",
                                 Toast.LENGTH_SHORT).show();
                         updateUI(null);
                     }
 
-                    // [START_EXCLUDE]
                     hideProgressDialog();
-                    // [END_EXCLUDE]
                 });
-        // [END create_user_with_email]
     }
 
     private void signInWithEmailPassword(String email, String password) {
@@ -134,31 +114,22 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         showProgressDialog();
 
 
-        // [START sign_in_with_email]
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success");
                         FirebaseUser user = mAuth.getCurrentUser();
                         updateUI(user);
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.getException());
                         Toast.makeText(MainActivity.this, "Authentication failed.",
                                 Toast.LENGTH_SHORT).show();
                         updateUI(null);
                     }
 
-                    // [START_EXCLUDE]
                     if (!task.isSuccessful()) {
                         Toast.makeText(this, R.string.auth_failed, Toast.LENGTH_SHORT).show();
-//                        mStatusTextView.setText(R.string.auth_failed);
                     }
                     hideProgressDialog();
-                    // [END_EXCLUDE]
                 });
-        // [END sign_in_with_email]
     }
 
     private void signOut() {
@@ -190,28 +161,22 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private void signInWithGoogle(GoogleSignInAccount acct) {
         Log.d(TAG, "signInWithGoogle:" + acct.getId());
-        // [START_EXCLUDE silent]
         showProgressDialog();
-        // [END_EXCLUDE]
 
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
                         Log.d(TAG, "signInWithCredential:success");
                         FirebaseUser user = mAuth.getCurrentUser();
                         updateUI(user);
                     } else {
-                        // If sign in fails, display a message to the user.
                         Log.w(TAG, "signInWithCredential:failure", task.getException());
                         Toast.makeText(MainActivity.this, "signInWithCredential:failure", Toast.LENGTH_SHORT).show();
                         updateUI(null);
                     }
 
-                    // [START_EXCLUDE]
                     hideProgressDialog();
-                    // [END_EXCLUDE]
                 });
     }
 
@@ -219,7 +184,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
